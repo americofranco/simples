8df734c5ba03f55fa1b67f02888c6794df590a2c
americofranco <americofranco@gmail.com>
2022-08-30 21:49:24 -0300

    B30082022

--- connections/database.js
+++ connections/database.js
@@ -5,8 +5,24 @@ const Sequelize = require('sequelize');
 //const sequelize = new Sequelize('postgres://root:postgres@localhost:5432/postgres');
 
 //Heroku
-const  sequelize = new Sequelize('postgres://zrkcvgistijgus:9452e5c78cfe6ffd32a95cf1a6c17b252b93cdeb15523e299b7ac0bf65db1c29@ec2-44-206-11-200.compute-1.amazonaws.com:5432/dd6rahakq2ptgu');
+sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+);
 
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 
 
